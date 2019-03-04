9c55cf49ee529f4d3ed557ebf951caf19149e839
nak435 <42625437+nak435@users.noreply.github.com>
2019-03-04 16:48:34 +0900

    Update UnionFind.js

--- UnionFind.js
+++ UnionFind.js
@@ -2,7 +2,8 @@
 
 class UnionFind {
     constructor(n) {
-        this.par = new Array(n).fill(0).map((v, i) => i);
+        this.rnk = new Array(n).fill(0);
+        this.par = this.rnk.map((v, i) => i);
     }
     root(x) {
         if (this.par[x] == x) return x;
@@ -12,13 +13,24 @@ class UnionFind {
         let rx = this.root(x);
         let ry = this.root(y);
         if (rx == ry) return;
-        this.par[rx] = ry;
+
+        if (this.rnk[rx] < this.rnk[ry]) {
+            this.par[rx] = ry;
+        } else {
+            this.par[ry] = rx;
+            if (this.rnk[rx] == this.rnk[ry]) this.rnk[rx] += 1;
+        }
     }
     same(x, y) {
         let rx = this.root(x);
         let ry = this.root(y);
         return rx == ry;
     }
+    size(x) {
+        return this.rnk[this.root(x)];
+    }
+    find(x) { return this.root(x); }
+    union(x, y) { this.unite(x, y); }
 }
 
 /* コメント有り
@@ -26,7 +38,8 @@ class UnionFind {
     // par[i]:iの親の番号　(例) par[3] = 2 : 3の親が2
 
     constructor(n) { //最初は全てが根であるとして初期化
-        this.par = new Array(n).fill(0).map((v, i) => i);
+        this.rnk = new Array(n).fill(0); // 親
+        this.par = rnk.map((v, i) => i); // 深さ
     }
 
     root(x) { // データxが属する木の根を再帰で得る：root(x) = {xの木の根}
@@ -38,7 +51,13 @@ class UnionFind {
         let rx = this.root(x); //xの根をrx
         let ry = this.root(y); //yの根をry
         if (rx == ry) return; //xとyの根が同じ(=同じ木にある)時はそのまま
-        this.par[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+        //this.par[rx] = ry; //xとyの根が同じでない(=同じ木にない)時：xの根rxをyの根ryにつける
+        if (this.rnk[rx] < this.rnk[ry]) {
+            this.par[rx] = ry;
+        } else {
+            this.par[ry] = rx;
+            if (this.rnk[rx] == this.rnk[ry]) this.rnk[rx] += 1;
+        }        
     }
 
     same(x, y) { // 2つのデータx, yが属する木が同じならtrueを返す
@@ -46,5 +65,12 @@ class UnionFind {
         let ry = this.root(y);
         return rx == ry;
     }
+
+    size(x) { // データxが含まれる木の大きさを返す
+        return this.rnk[this.root(x)];
+    }
 }
 */
+
+//各種アルゴリズムの C++ による実装
+//http://www.prefield.com/algorithm/index.html
