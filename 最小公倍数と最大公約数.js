cf93fcc5657bfbc288b92d48b3f3949ad8a1de35
nak435 <42625437+nak435@users.noreply.github.com>
2019-05-04 20:15:38 +0900

    Update 最小公倍数と最大公約数.js

--- 最小公倍数と最大公約数.js
+++ 最小公倍数と最大公約数.js
@@ -4,7 +4,7 @@ const gcd = (a, b) => b ? gcd(b, a % b) : a;
 const lcm = (a, b) => a * b / gcd(a, b);
 
 //最大公約数（配列版）
-function gcd(v) {
+const gcd = function(v) {
     const g = (a, b) => b ? g(b, a % b) : a;
     let r = v[0];
     for (let i = 1; i < v.length; i++) {
@@ -14,7 +14,7 @@ function gcd(v) {
 }
 
 //最小公倍数（配列版）
-function lcm(v) {
+const lcm = function(v) {
     const g = (a, b) => b ? g(b, a % b) : a;
     const l = (a, b) => a * b / g(a, b);
     let r = v[0];
@@ -25,7 +25,7 @@ function lcm(v) {
 } 
 
 //素因数分解
-function prime(v) {
+const prime = function(v) {
     let a = v, i, r = [];
     for (let j = 2; j <= Math.floor(v / 2) && 1 < a; j += 1 + (j & 1)) {
         for (i = 0; a % j == 0; i++) a = Math.floor(a / j);
@@ -34,3 +34,16 @@ function prime(v) {
     if (r.length == 0) r.push([v, 1]);
     return r;
 } //30=>2^1*3^1*5^1=>[[2,1],[3,1],[5,1]]
+
+//順列
+const P = function(n, r) {
+    let ans = 1;
+    for (let m = n; r > 0; m--, r--) ans *= m;
+    return ans;
+}
+//組合せ
+const C = function(n, r) {
+    let ans = P(n, r);
+    for (; r > 0; r--) ans /= r;
+    return ans;
+}
