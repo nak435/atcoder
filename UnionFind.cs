28e573354f9ef71f308da439ef15ffa270de4aef
nak435 <42625437+nak435@users.noreply.github.com>
2019-05-24 14:41:42 +0900

    Update UnionFind.cs

--- UnionFind.cs
+++ UnionFind.cs
@@ -1,35 +1,40 @@
-public class UnionFind {
-    public int[] rnk, par;
-    public UnionFind(int n) {
-        this.rnk = new int[n];
-        this.par = new int[n];
-        for (var i = 0; i < n; i++) this.par[i] = i;
-    }
-    public int root(int x) {
-        if (this.par[x] == x) return x;
-        return this.par[x] = this.root(this.par[x]);
-    }
-    public bool unite(int x, int y) {
-        var rx = this.root(x);
-        var ry = this.root(y);
-        if (rx == ry) return false;
-
-        if (this.rnk[rx] < this.rnk[ry]) {
-            this.par[rx] = ry;
-        } else {
-            this.par[ry] = rx;
-            if (this.rnk[rx] == this.rnk[ry]) this.rnk[rx] += 1;
+    public class UnionFind {
+        public int[] rank, par, size;
+        public UnionFind(int n) {
+            this.rank = new int[n];
+            this.par = new int[n];
+            this.size = new int[n];
+            for (var i = 0; i < n; i++) {
+                this.par[i] = i;
+                this.size[i] = 1;
+            }
         }
-        return true;
+        public int find(int x) {
+            if (this.par[x] == x) return x;
+            return this.par[x] = this.find(this.par[x]);
+        }
+        public bool union(int x, int y) {
+            var rx = this.root(x);
+            var ry = this.root(y);
+            if (rx == ry) return false;
+
+            if (this.rank[rx] < this.rank[ry]) {
+                int w = rx;
+                rx = ry;
+                ry = w;
+            } else if (this.rank[rx] == this.rank[ry]) {
+                this.rank[rx] += 1;
+            }
+            this.par[ry] = rx;
+            this.size[rx] += this.size[ry];
+            return true;
+        }
+        public bool isSame(int x, int y) {
+            return this.root(x) == this.root(y);
+        }
+        public int getSize(int x) {
+            return this.size[this.find(x)];
+        }
+        public int root(int x) { return this.find(x); }
+        public bool unite(int x, int y) { return this.union(x, y); }
     }
-    public bool same(int x, int y) {
-        var rx = this.root(x);
-        var ry = this.root(y);
-        return rx == ry;
-    }
-    public int size(int x) {
-        return this.rnk[this.root(x)];
-    }
-    public int find(int x) { return this.root(x); }
-    public void union(int x, int y) { this.unite(x, y); }
-}
